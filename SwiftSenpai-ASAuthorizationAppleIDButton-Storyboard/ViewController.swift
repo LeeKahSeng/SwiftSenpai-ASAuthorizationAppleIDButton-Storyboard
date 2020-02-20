6df1e0791386c3115436909aae3d020b26657ca2
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-02-20 16:05:55 +0800

    Initial commit

---
+++ SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard
+//
+//  Created by Lee Kah Seng on 20/02/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
