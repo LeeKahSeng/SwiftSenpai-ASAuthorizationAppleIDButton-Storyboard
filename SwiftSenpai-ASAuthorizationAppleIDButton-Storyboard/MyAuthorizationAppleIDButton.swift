a5d24188df6e156c0934b48e30742204f687f4db
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-02-20 18:22:44 +0800

    Added ASAuthorizationAppleIDButton as subview

--- SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard/MyAuthorizationAppleIDButton.swift
+++ SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard/MyAuthorizationAppleIDButton.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 @IBDesignable
 class MyAuthorizationAppleIDButton: UIButton {
 
+    private var authorizationButton: ASAuthorizationAppleIDButton!
+    
     override public init(frame: CGRect) {
         super.init(frame: frame)
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override public func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        // Create ASAuthorizationAppleIDButton
+        authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
+
+        // Disable user interaction so that it won't hijack the tap gesture towards MyAuthorizationAppleIDButton
+        authorizationButton.isUserInteractionEnabled = false
+
+        // Show authorizationButton
+        addSubview(authorizationButton)
+
+        // Use autolayout to make authorizationButton follow the MyAuthorizationAppleIDButton's dimension
+        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            authorizationButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 0.0),
+            authorizationButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0.0),
+            authorizationButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0.0),
+            authorizationButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0.0),
+        ])
     }
 
 }
