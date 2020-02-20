58126bf4baa3dc7fff81723cb43a9bc6920421d6
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-02-20 17:33:58 +0800

    Added UIButton subclass

---
+++ SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard/MyAuthorizationAppleIDButton.swift
@@ -0,0 +1,18 @@
+//
+//  MyAuthorizationAppleIDButton.swift
+//  SwiftSenpai-ASAuthorizationAppleIDButton-Storyboard
+//
+//  Created by Lee Kah Seng on 20/02/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class MyAuthorizationAppleIDButton: UIButton {
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
